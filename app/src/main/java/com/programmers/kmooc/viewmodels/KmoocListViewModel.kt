13400bdaf783c7f18e9199b5dda934ab242fc319
k906506 <k906506@naver.com>
2022-05-14 15:47:43 +0900

    [feat] Observer 패턴 구현을 위한 ProgressBar, 강의 목록 갱신 함수 구현

--- app/src/main/java/com/programmers/kmooc/viewmodels/KmoocListViewModel.kt
+++ app/src/main/java/com/programmers/kmooc/viewmodels/KmoocListViewModel.kt
@@ -1,16 +1,26 @@
 package com.programmers.kmooc.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.programmers.kmooc.models.Lecture
 import com.programmers.kmooc.models.LectureList
 import com.programmers.kmooc.repositories.KmoocRepository
 import java.util.Collections.addAll
 
 class KmoocListViewModel(private val repository: KmoocRepository) : ViewModel() {
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> get() = _isLoading
+
+    private val _lectures = MutableLiveData<LectureList>()
+    val lectures: LiveData<LectureList> get() = _lectures
 
     fun list() {
+        _isLoading.postValue(true)
         repository.list { lectureList ->
+            _lectures.postValue(lectureList)
+            _isLoading.postValue(false)
         }
     }
 
