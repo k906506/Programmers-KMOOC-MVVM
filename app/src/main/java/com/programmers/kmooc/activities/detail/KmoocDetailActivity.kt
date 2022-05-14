89e023d9133c35769ea7ef2c088ee704da2c5969
k906506 <k906506@naver.com>
2022-05-14 16:47:33 +0900

    [feat] 강의 상세 정보 페이지 구현

--- app/src/main/java/com/programmers/kmooc/activities/detail/KmoocDetailActivity.kt
+++ app/src/main/java/com/programmers/kmooc/activities/detail/KmoocDetailActivity.kt
@@ -31,5 +31,45 @@ class KmoocDetailActivity : AppCompatActivity() {
 
         binding = ActivityKmookDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViews()
+        subscribeObserver()
+    }
+
+    private fun initViews() {
+        val courseId = intent.getStringExtra(INTENT_PARAM_COURSE_ID)
+
+        if (courseId.isNullOrEmpty()) {
+            finish()
+            return
+        } else {
+            viewModel.detail(courseId)
+        }
+    }
+
+    private fun bindViews(lecture: Lecture) = with(binding) {
+        toolbar.title = lecture.name
+        ImageLoader.loadImage(lecture.courseImage) {
+            lectureImage.setImageBitmap(it)
+        }
+        lectureNumber.setDescription("강좌번호 : ", lecture.number)
+        lectureType.setDescription(
+            "강좌분류 : ",
+            "${lecture.classfyName} (${lecture.middleClassfyName})"
+        )
+        lectureOrg.setDescription("운영기관 : ", lecture.orgName)
+        lectureTeachers.setDescription("교수정보 : ", lecture.teachers ?: "교수 정보가 없습니다.")
+        lectureDue.setDescription(
+            "운영기간 : ",
+            DateUtil.formatDate(lecture.start) + " ~ " + DateUtil.formatDate(lecture.end)
+        )
+    }
+
+    private fun subscribeObserver() {
+        viewModel.lecture.observe(this) { bindViews(it) }
+        viewModel.isLoading.observe(this) {
+            binding.progressBar.visibility = it.toVisibility()
+        }
+        binding.toolbar.setNavigationOnClickListener { finish() }
     }
 }
