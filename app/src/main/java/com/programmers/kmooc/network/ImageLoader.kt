8008254ede27ab3dfb36276295119f7674e427a7
k906506 <k906506@naver.com>
2022-05-14 17:02:19 +0900

    [feat] 강의 목록에서 이미지 로딩 관련 오류 이미지 캐싱으로 해결

--- app/src/main/java/com/programmers/kmooc/network/ImageLoader.kt
+++ app/src/main/java/com/programmers/kmooc/network/ImageLoader.kt
@@ -2,21 +2,29 @@ package com.programmers.kmooc.network
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import androidx.core.net.toUri
 import kotlinx.coroutines.*
 import java.lang.Exception
 import java.net.URL
 
 object ImageLoader {
+    private val imageCache = mutableMapOf<String, Bitmap>()
     fun loadImage(url: String, completed: (Bitmap?) -> Unit) {
         //TODO: String -> Bitmap 을 구현하세요
         if (url.isNullOrEmpty()) {
             completed(null)
+            return
+        }
+
+        if (imageCache.containsKey(url)) {
+            completed(imageCache[url])
+            return
         }
 
         CoroutineScope(Dispatchers.IO).launch {
             try {
                 val bitmap = BitmapFactory.decodeStream(URL(url).openStream())
+                // 이미지 캐싱
+                imageCache[url] = bitmap
                 // 완료되면 비트맵을 반환
                 withContext(Dispatchers.Main) {
                     completed(bitmap)
