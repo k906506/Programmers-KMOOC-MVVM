dd25746a30cf31c92bd31d52992290b9c11067fb
k906506 <k906506@naver.com>
2022-05-14 14:52:03 +0900

    [feat] Image Loader 구현

--- app/src/main/java/com/programmers/kmooc/network/ImageLoader.kt
+++ app/src/main/java/com/programmers/kmooc/network/ImageLoader.kt
@@ -2,15 +2,31 @@ package com.programmers.kmooc.network
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import androidx.core.net.toUri
+import kotlinx.coroutines.*
 import java.lang.Exception
 import java.net.URL
 
 object ImageLoader {
     fun loadImage(url: String, completed: (Bitmap?) -> Unit) {
         //TODO: String -> Bitmap 을 구현하세요
+        if (url.isNullOrEmpty()) {
+            completed(null)
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val bitmap = BitmapFactory.decodeStream(URL(url).openStream())
+                // 완료되면 비트맵을 반환
+                withContext(Dispatchers.Main) {
+                    completed(bitmap)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                withContext(Dispatchers.Main) {
+                    completed(null)
+                }
+            }
+        }
     }
 }
